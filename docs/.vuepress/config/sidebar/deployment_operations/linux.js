80b57725962fb247c760fa770680bd1ecca19c1a
Kenyon Li <kenyonli@outlook.com>
2024-05-04 02:48:13 +0800

    One click submission blog file

--- docs/.vuepress/config/sidebar/deployment_operations/linux.js
+++ docs/.vuepress/config/sidebar/deployment_operations/linux.js
@@ -3,34 +3,34 @@
 module.exports = [
     {
         text: 'linux基础',
-        link: '/technology/linux/index.md',
+        link: '/technology/deployment_operations/linux/index.md',
     },
     {
         text: 'linux（centos9）root密码忘记了，怎么重置或修改密码',
-        link: '/technology/linux/centos9_001.md',
+        link: '/technology/deployment_operations/linux/centos9_001.md',
     },
     {
         text: 'Ubuntu20.4 搭建vpn服务器',
-        link: '/technology/linux/vpn01.md',
+        link: '/technology/deployment_operations/linux/vpn01.md',
     },
     {
         text: 'Linux实现socks终端代理、全局代理',
-        link: '/technology/linux/vpn02.md',
+        link: '/technology/deployment_operations/linux/vpn02.md',
     },
     {
         text: '搭建 vpn 使用 x-ui 部署配置',
-        link: '/technology/linux/vpn03.md',
+        link: '/technology/deployment_operations/linux/vpn03.md',
     },
     {
         text: 'yum及yum-config-manager命令详解',
-        link: '/technology/linux/linux001.md',
+        link: '/technology/deployment_operations/linux/linux001.md',
     },
     {
         text: 'dnf命令详解',
-        link: '/technology/linux/linux002.md',
+        link: '/technology/deployment_operations/linux/linux002.md',
     },
     {
         text: 'linux 运行 AppImage 应用',
-        link: '/technology/linux/linux003.md',
+        link: '/technology/deployment_operations/linux/linux003.md',
     }
 ]
